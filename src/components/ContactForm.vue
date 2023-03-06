dfdc4a5df2b79a76a1d5759a085340cb7dd64aa6
RealWooblay <jackaustinc66@gmail.com>
2023-03-06 23:12:41 +1100

    PageFix

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,6 +1,4 @@
 <template>
-  <div class="main">
-    <div class="col">
       <form @submit.prevent="submitForm">
         <label>
           Name:
@@ -16,21 +14,6 @@
         </label>
         <button type="submit">Submit</button>
       </form>
-    </div>
-    <div class="col1">
-      <h1>Contact Information</h1>
-      <br>
-      <h3>Email: ...</h3>
-      <br>
-      <h3>Address: ...</h3>
-      <br>
-      <h4>Thank you for Contacting Us! We will get back to you as soon as possible.
-      <br> We are excited to take this journey with you to achieving your dreams.
-      </h4>
-      <br>
-      <h1 style="color: darkred;">- Fairlight Ventures</h1>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -98,17 +81,6 @@ button[type='submit'] {
   border-radius: 0.25rem;
   padding: 0.5rem 1rem;
   cursor: pointer;
-}
-
-.main {
-  padding: 70px;
-  background: linear-gradient(135deg, #e4e4e4, #9b9b9b, #e4e4e4);
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-
-.col1 {
-  padding-left: 80px;
 }
 
 
