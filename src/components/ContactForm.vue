1d24162bfc5eeab5b8eaf53ca05534b65cf55029
RealWooblay <jackaustinc66@gmail.com>
2023-03-06 23:06:10 +1100

    cont

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,20 +1,37 @@
 <template>
-    <form @submit.prevent="submitForm">
-      <label>
-        Name:
-        <input type="text" v-model="name" />
-      </label>
-      <label>
-        Email:
-        <input type="email" v-model="email" />
-      </label>
-      <label>
-        Message:
-        <textarea v-model="message"></textarea>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  </template>
+  <div class="main">
+    <div class="col">
+      <form @submit.prevent="submitForm">
+        <label>
+          Name:
+          <input type="text" v-model="name" />
+        </label>
+        <label>
+          Email:
+          <input type="email" v-model="email" />
+        </label>
+        <label>
+          Message:
+          <textarea v-model="message"></textarea>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    <div class="col1">
+      <h1>Contact Information</h1>
+      <br>
+      <h3>Email: ...</h3>
+      <br>
+      <h3>Address: ...</h3>
+      <br>
+      <h4>Thank you for Contacting Us! We will get back to you as soon as possible.
+      <br> We are excited to take this journey with you to achieving your dreams.
+      </h4>
+      <br>
+      <h1 style="color: darkred;">- Fairlight Ventures</h1>
+    </div>
+  </div>
+</template>
 
 <script>
   import emailjs from 'emailjs-com'
@@ -55,6 +72,11 @@
 </script>
 
 <style>
+
+.main {
+  height: 550px;
+}
+
 label {
   display: block;
   margin-bottom: 0.5rem;
@@ -77,4 +99,17 @@ button[type='submit'] {
   padding: 0.5rem 1rem;
   cursor: pointer;
 }
+
+.main {
+  padding: 70px;
+  background: linear-gradient(135deg, #e4e4e4, #9b9b9b, #e4e4e4);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.col1 {
+  padding-left: 80px;
+}
+
+
 </style>
