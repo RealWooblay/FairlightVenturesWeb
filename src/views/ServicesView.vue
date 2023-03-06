fee84d20c90d09d8fb389bd906ab0f3fff023c5f
RealWooblay <jackaustinc66@gmail.com>
2023-03-06 22:51:34 +1100

    b

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="main">
-        <Slider/>
+        <ServicesCard/>
         <div class="seperator"></div>
         <Service 
-        imagePath="src\assets\Services\BC.jpg" 
-        title="Investment" 
-        subText="We back the best and brightest in emerging technologies. We are not just investing capital
-         - we are investing our resources, time, and connections to help accelerate growth and solve problems. Our portfolio spans some of the most exciting areas of innovation,
-          including cyber security, space tech, quantum technologies, and blockchain. We don’t just invest in ideas, we invest in the people behind them."
-        position="right"/>
-        <div class="seperator"></div>
-        <Service 
-        imagePath="src\assets\Services\BandWCity.png" 
+        imagePath="src\assets\Services\HS.jpg" 
         title="Advisory" 
         subText="When it comes to cyber security, business operations, and capital raising, we know our stuff. At Fairlight Ventures, 
                 our technical advisory boards and non-executive directorships provide unparalleled subject matter expertise to companies looking to take their game to the next level. 
                 We work closely with our clients to understand their unique needs and tailor our approach to provide the best possible outcome."
+        position="right"/>
+        <div class="seperator"></div>
+        <Service 
+        imagePath="src\assets\Services\AI.jpg" 
+        title="Investment" 
+        subText="We back the best and brightest in emerging technologies. We are not just investing capital
+         - we are investing our resources, time, and connections to help accelerate growth and solve problems. Our portfolio spans some of the most exciting areas of innovation,
+          including cyber security, space tech, quantum technologies, and blockchain. We don’t just invest in ideas, we invest in the people behind them."
         position="left"/>
         <div class="seperator">
             <router-link to="/contact">
@@ -23,7 +23,7 @@
             </router-link>
         </div>
         <Service 
-        imagePath="src\assets\Services\BC.jpg" 
+        imagePath="src\assets\Services\BandWCity.png" 
         title="Consulting" 
         subText="If you're looking to design and improve your operations, or develop a winning strategy and execute it flawlessly, Fairlight Ventures has your back. 
                 Our team can help you identify areas of improvement, develop a roadmap for success, and provide ongoing support to help you achieve your goals. 
@@ -31,7 +31,7 @@
         position="right"/>
         <div class="seperator"></div>
         <Service 
-        imagePath="src\assets\Services\BC.jpg" 
+        imagePath="src\assets\Services\PB.jpg" 
         title="Public engagements" 
         subText="We are passionate about sharing our expertise and thought leadership with the broader community. Whether it's keynotes, panels, or other speaking engagements, 
                 we're always happy to engage and inspire audiences of all types. From emerging technologies to cyber security and beyond, we bring our A-game to every engagement to 
@@ -48,18 +48,20 @@
 </template>
 
 <script setup>
-import Slider from '../components/Slider.vue';
+import ServicesCard from '../components/ServicesCard.vue';
 import Service from '../components/Storybook/Service.vue';
 import Button from '../components/Storybook/Button.vue';
 import Footer from '../components/Footer.vue';
 </script>
 
 <style scoped>
+
 .seperator {
-    margin-top: 50px;
-    margin-bottom: 50px;
+    padding-top: 50px;
+    padding-bottom: 50px;
     display: flex;
     justify-content: center;
+    background: linear-gradient(135deg, #e4e4e4, #9b9b9b, #e4e4e4);
 }
 
 </style>
